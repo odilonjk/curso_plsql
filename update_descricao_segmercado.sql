a126972094f83be8b0d77152f6d741c7fb0bb54a
odilonjk <kroger.odilonj@gmail.com>
2018-01-04 18:19:50 -0200

    Realizando DELETE dentro do bloco.

--- update_descricao_segmercado.sql
+++ update_descricao_segmercado.sql
@@ -17,3 +17,19 @@ BEGIN
 END;
 
 SELECT * FROM SEGMERCADO;
+
+DECLARE
+    v_id segmercado.id%type := 3;
+    v_descricao segmercado.descricao%type := 'esportivo';
+BEGIN
+    INSERT INTO segmercado VALUES (v_id, UPPER(v_descricao));
+    COMMIT;
+END;
+
+DECLARE
+    v_id segmercado.id%type := 3;
+BEGIN
+    DELETE FROM segmercado
+        WHERE id = v_id;
+    COMMIT;
+END;
