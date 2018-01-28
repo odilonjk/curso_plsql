7fb20dcbc3f8838fd5243c8662fa2299c01064f8
odilonjk <kroger.odilonj@gmail.com>
2018-01-28 12:52:52 -0200

    Adicionando tratamento de Exception.

--- procedure_incluir_cliente.sql
+++ procedure_incluir_cliente.sql
@@ -7,6 +7,9 @@ CREATE OR REPLACE PROCEDURE incluir_cliente
 IS
     v_categoria cliente.categoria%type;
     v_cnpj cliente.cnpj%type := p_cnpj;
+    --Componente exception
+    e_null EXCEPTION;
+    PRAGMA exception_init (e_null, -1400);
 BEGIN
     v_categoria := categoria_cliente(p_faturamento_previsto);
     formatar_cnpj(v_cnpj);
@@ -14,6 +17,18 @@ BEGIN
         VALUES (p_id, UPPER(p_razao_social), v_cnpj, p_segmercado_id, 
                 SYSDATE, p_faturamento_previsto, v_categoria);
     COMMIT;
+
+    EXCEPTION
+        --Tratando a exception de primary key.
+        WHEN dup_val_on_index THEN
+            raise_application_error(-20010,'ID já esta em uso.');
+        --Tratando a exception de NULL.                
+        WHEN e_null THEN
+            raise_application_error(-20015, 'A coluna ID tem preenchimento obrigatório.');
+        -- Erro others pega erros não previstos. Sempre precisa ser o último.
+        WHEN others THEN
+            raise_application_error(-20020, 'Erro ocorrido: ' || sqlerrm());
+            
 END;
 
 EXECUTE INCLUIR_CLIENTE(3, 'ATLASSIAN', '12121212', 4, 400000);
